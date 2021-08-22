1624e9d3b1cda57f2d029be845a55bc5c487d63f
e1m3m0 <gamateo@gmail.com>
2021-08-22 12:18:12 -0400

    html containers and structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
-var score = 0;
+var timeCounter = document.querySelector("#counter");
+var timeLeft = 0;
+
+var quizTimer = function () {
+  timeLeft = 75;
+
+  var timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+      timeLeft--;
+      var countDown = "Time left " + timeLeft;
+      timeCounter.textContent = countDown;
+    } else if (timeLeft === 1) {
+      timeLeft--;
+      var countDown = "Time left " + timeLeft;
+      timeCounter.textContent = countDown;
+      alert("Your time has ended, click ok to see your score.");
+    }
+  }, 1000);
+};
+
+var questionsContainer = document.querySelector("#questions");
+var answersContainer = document.querySelector("#answers");
+var answersResponseContainer = document.querySelector("#answer-response");
+
+questionsContainer.innerHTML = "<h1>Coding Quiz Challenge</h1>";
+answersContainer.innerHTML =
+  "<p>Try to answer the following code-related questions within the time limit <br> Keep in mind that incorrect answers will penalize your score/time <br>By ten seconds!</p>";
+
+var startQuizBtn = document.createElement("button");
+startQuizBtn.className = "start-quiz btn";
+startQuizBtn.textContent = "Start Quiz";
+
+console.log(startQuizBtn);
+answersResponseContainer.appendChild(startQuizBtn);
